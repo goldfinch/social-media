11838363d7275a4b3b4b9b17a0d528cb16a6bd13
Art Karasev <as.karasyov@gmail.com>
2023-09-29 14:24:05 +1300

    add command

---
+++ src/Commands/GenerateEncryptionKeyCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Goldfinch\SocialKit\Commands;
+
+use LeKoala\Encrypt\EncryptHelper;
+use Goldfinch\Taz\Services\InputOutput;
+use Goldfinch\Taz\Console\GeneratorCommand;
+use Symfony\Component\Console\Command\Command;
+
+#[AsCommand(name: 'app:generate-encryption-key')]
+class GenerateEncryptionKeyCommand extends GeneratorCommand
+{
+    protected static $defaultName = 'app:generate-encryption-key';
+
+    protected $description = 'Generate Encryption Key (lekoala/silverstripe-encrypt)';
+
+    protected function execute($input, $output): int
+    {
+        // parent::execute($input, $output);
+
+        $io = new InputOutput($input, $output);
+        $io->text(EncryptHelper::generateKey());
+
+        return Command::SUCCESS;
+    }
+}
