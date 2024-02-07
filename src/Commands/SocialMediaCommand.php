cedbf2b928ed0cfd7958dd1bd0e054047b556518
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-07 21:32:27 +1300

    WIP

--- src/Commands/SocialMediaCommand.php
+++ src/Commands/SocialMediaCommand.php
@@ -2,12 +2,8 @@
 
 namespace Goldfinch\SocialMedia\Commands;
 
-use Symfony\Component\Finder\Finder;
-use Goldfinch\Taz\Services\InputOutput;
+use Goldfinch\Taz\Services\Templater;
 use Goldfinch\Taz\Console\GeneratorCommand;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Question\ChoiceQuestion;
 
 #[AsCommand(name: 'vendor:social-media:templates')]
 class SocialMediaCommand extends GeneratorCommand
@@ -20,73 +16,36 @@ class SocialMediaCommand extends GeneratorCommand
 
     protected function execute($input, $output): int
     {
-        $io = new InputOutput($input, $output);
+        $templater = Templater::create($input, $output, $this, 'goldfinch/social-media');
 
-        $themes = Finder::create()
-            ->in(THEMES_PATH)
-            ->depth(0)
-            ->directories();
+        $theme = $templater->defineTheme();
 
-        $ssTheme = null;
+        if (is_string($theme)) {
 
-        if (!$themes || !$themes->count()) {
-            $io->wrong('Themes not found');
+            $componentPathTemplates = BASE_PATH . '/vendor/goldfinch/social-media/templates/';
+            $componentPath = $componentPathTemplates . 'Goldfinch/Component/Products/';
+            $themeTemplates = 'themes/' . $theme . '/templates/';
+            $themePath = $themeTemplates . 'Goldfinch/Component/Products/';
 
-            return Command::SUCCESS;
-        } elseif ($themes->count() > 1) {
-            // choose theme
+            $files = [
+                [
+                    'from' => $componentPathTemplates . 'Views/SocialFeed.ss',
+                    'to' => $themeTemplates . 'Views/SocialFeed.ss',
+                ],
+                [
+                    'from' => $componentPathTemplates . 'Views/FacebookFeed.ss',
+                    'to' => $themeTemplates . 'Views/FacebookFeed.ss',
+                ],
+                [
+                    'from' => $componentPathTemplates . 'Views/InstagramFeed.ss',
+                    'to' => $themeTemplates . 'Views/InstagramFeed.ss',
+                ],
 
-            $availableThemes = [];
+            ];
 
-            foreach ($themes as $theme) {
-                $availableThemes[] = $theme->getFilename();
-            }
-
-            $helper = $this->getHelper('question');
-            $question = new ChoiceQuestion(
-                'Which templete?',
-                $availableThemes,
-                0,
-            );
-            $question->setErrorMessage('Theme %s is invalid.');
-            $theme = $helper->ask($input, $output, $question);
+            return $templater->copyFiles($files);
         } else {
-            foreach ($themes as $themeItem) {
-                $theme = $themeItem->getFilename();
-            }
+            return $theme;
         }
-
-        if (isset($theme) && $theme) {
-            $this->copyTemplates($theme);
-
-            $io->right('The [social-media] templates have been created');
-
-            return Command::SUCCESS;
-        }
-
-        $io->wrong('The [social-media] templates creation failed');
-
-        return Command::FAILURE;
-    }
-
-    private function copyTemplates($theme)
-    {
-        $fs = new Filesystem();
-
-        $fs->copy(
-            BASE_PATH .
-                '/vendor/goldfinch/social-media/templates/Views/SocialFeed.ss',
-            'themes/' . $theme . '/templates/Views/SocialFeed.ss',
-        );
-        $fs->copy(
-            BASE_PATH .
-                '/vendor/goldfinch/social-media/templates/Views/FacebookFeed.ss',
-            'themes/' . $theme . '/templates/Views/FacebookFeed.ss',
-        );
-        $fs->copy(
-            BASE_PATH .
-                '/vendor/goldfinch/social-media/templates/Views/InstagramFeed.ss',
-            'themes/' . $theme . '/templates/Views/InstagramFeed.ss',
-        );
     }
 }
