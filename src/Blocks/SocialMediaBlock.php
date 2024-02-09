1741c2e02fada524273240855d35a016bb0a50fd
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-10 10:39:48 +1300

    WIP

--- src/Blocks/SocialMediaBlock.php
+++ src/Blocks/SocialMediaBlock.php
@@ -19,24 +19,8 @@ class SocialMediaBlock extends BaseElement
     private static $description = '';
     private static $icon = 'font-icon-block-instagram';
 
-    private static $has_one = [];
-
-    private static $owns = [];
-
     private static $field_labels = [
         'FeedType' => 'Type',
         'FeedLimit' => 'Post limit',
     ];
-
-    public function getSummary()
-    {
-        return $this->getDescription();
-    }
-
-    public function getType()
-    {
-        $default = $this->i18n_singular_name() ?: 'Block';
-
-        return _t(__CLASS__ . '.BlockType', $default);
-    }
 }
